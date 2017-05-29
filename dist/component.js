d3e35ef3b82320a86541ae579f152ce9c4b65d26
Mitchell Hewes <me@mitcdh.com>
2017-05-29 11:27:46 +1000

    Release at Mon 29 May 2017 11:27:46 AEST

--- dist/component.js
+++ dist/component.js
@@ -12,6 +12,7 @@ define('ui/components/machine/driver-scaleway/component', ['exports', 'ember', '
         type        : 'scalewayConfig',
         commercialType: 'VC1S',
         region: '',
+        name: '',
         debug: false,
         image: 'ubuntu-xenial',
         ip: '',
@@ -86,7 +87,7 @@ exports["default"] = Ember.HTMLBars.template((function() {
           "column": 0
         },
         "end": {
-          "line": 124,
+          "line": 139,
           "column": 0
         }
       }
@@ -202,6 +203,63 @@ exports["default"] = Ember.HTMLBars.template((function() {
       dom.appendChild(el4, el5);
       var el5 = dom.createElement("label");
       dom.setAttribute(el5,"class","form-control-static");
+      var el6 = dom.createTextNode("Name");
+      dom.appendChild(el5, el6);
+      dom.appendChild(el4, el5);
+      var el5 = dom.createTextNode("\n      ");
+      dom.appendChild(el4, el5);
+      dom.appendChild(el3, el4);
+      var el4 = dom.createTextNode("\n      ");
+      dom.appendChild(el3, el4);
+      var el4 = dom.createElement("div");
+      dom.setAttribute(el4,"class","col-md-4");
+      var el5 = dom.createTextNode("\n        ");
+      dom.appendChild(el4, el5);
+      var el5 = dom.createComment("");
+      dom.appendChild(el4, el5);
+      var el5 = dom.createTextNode("\n      ");
+      dom.appendChild(el4, el5);
+      dom.appendChild(el3, el4);
+      var el4 = dom.createTextNode("\n      ");
+      dom.appendChild(el3, el4);
+      var el4 = dom.createElement("div");
+      dom.setAttribute(el4,"class","col-md-2 form-label");
+      var el5 = dom.createTextNode("\n        ");
+      dom.appendChild(el4, el5);
+      var el5 = dom.createElement("label");
+      dom.setAttribute(el5,"class","form-control-static");
+      var el6 = dom.createTextNode("Region");
+      dom.appendChild(el5, el6);
+      dom.appendChild(el4, el5);
+      var el5 = dom.createTextNode("\n      ");
+      dom.appendChild(el4, el5);
+      dom.appendChild(el3, el4);
+      var el4 = dom.createTextNode("\n      ");
+      dom.appendChild(el3, el4);
+      var el4 = dom.createElement("div");
+      dom.setAttribute(el4,"class","col-md-4");
+      var el5 = dom.createTextNode("\n        ");
+      dom.appendChild(el4, el5);
+      var el5 = dom.createComment("");
+      dom.appendChild(el4, el5);
+      var el5 = dom.createTextNode("\n      ");
+      dom.appendChild(el4, el5);
+      dom.appendChild(el3, el4);
+      var el4 = dom.createTextNode("\n    ");
+      dom.appendChild(el3, el4);
+      dom.appendChild(el2, el3);
+      var el3 = dom.createTextNode("\n    ");
+      dom.appendChild(el2, el3);
+      var el3 = dom.createElement("div");
+      dom.setAttribute(el3,"class","row form-group");
+      var el4 = dom.createTextNode("\n      ");
+      dom.appendChild(el3, el4);
+      var el4 = dom.createElement("div");
+      dom.setAttribute(el4,"class","col-md-2 form-label");
+      var el5 = dom.createTextNode("\n        ");
+      dom.appendChild(el4, el5);
+      var el5 = dom.createElement("label");
+      dom.setAttribute(el5,"class","form-control-static");
       var el6 = dom.createTextNode("Image");
       dom.appendChild(el5, el6);
       dom.appendChild(el4, el5);
@@ -259,31 +317,6 @@ exports["default"] = Ember.HTMLBars.template((function() {
       dom.appendChild(el4, el5);
       var el5 = dom.createElement("label");
       dom.setAttribute(el5,"class","form-control-static");
-      var el6 = dom.createTextNode("Region");
-      dom.appendChild(el5, el6);
-      dom.appendChild(el4, el5);
-      var el5 = dom.createTextNode("\n      ");
-      dom.appendChild(el4, el5);
-      dom.appendChild(el3, el4);
-      var el4 = dom.createTextNode("\n      ");
-      dom.appendChild(el3, el4);
-      var el4 = dom.createElement("div");
-      dom.setAttribute(el4,"class","col-md-4");
-      var el5 = dom.createTextNode("\n          ");
-      dom.appendChild(el4, el5);
-      var el5 = dom.createComment("");
-      dom.appendChild(el4, el5);
-      var el5 = dom.createTextNode("\n      ");
-      dom.appendChild(el4, el5);
-      dom.appendChild(el3, el4);
-      var el4 = dom.createTextNode("\n      ");
-      dom.appendChild(el3, el4);
-      var el4 = dom.createElement("div");
-      dom.setAttribute(el4,"class","col-md-2 form-label");
-      var el5 = dom.createTextNode("\n        ");
-      dom.appendChild(el4, el5);
-      var el5 = dom.createElement("label");
-      dom.setAttribute(el5,"class","form-control-static");
       var el6 = dom.createTextNode("Additional Volumes");
       dom.appendChild(el5, el6);
       dom.appendChild(el4, el5);
@@ -299,6 +332,19 @@ exports["default"] = Ember.HTMLBars.template((function() {
       var el5 = dom.createComment("");
       dom.appendChild(el4, el5);
       var el5 = dom.createTextNode("\n      ");
+      dom.appendChild(el4, el5);
+      dom.appendChild(el3, el4);
+      var el4 = dom.createTextNode("\n    ");
+      dom.appendChild(el3, el4);
+      dom.appendChild(el2, el3);
+      var el3 = dom.createTextNode("\n\n    ");
+      dom.appendChild(el2, el3);
+      var el3 = dom.createElement("div");
+      dom.setAttribute(el3,"class","over-hr r-mt20 r-mb20");
+      var el4 = dom.createTextNode("\n      ");
+      dom.appendChild(el3, el4);
+      var el4 = dom.createElement("span");
+      var el5 = dom.createTextNode("Network");
       dom.appendChild(el4, el5);
       dom.appendChild(el3, el4);
       var el4 = dom.createTextNode("\n    ");
@@ -374,7 +420,7 @@ exports["default"] = Ember.HTMLBars.template((function() {
       var el4 = dom.createTextNode("\n    ");
       dom.appendChild(el3, el4);
       dom.appendChild(el2, el3);
-      var el3 = dom.createTextNode("\n   \n    ");
+      var el3 = dom.createTextNode("\n    ");
       dom.appendChild(el2, el3);
       var el3 = dom.createElement("div");
       dom.setAttribute(el3,"class","row form-group");
@@ -440,8 +486,8 @@ exports["default"] = Ember.HTMLBars.template((function() {
       var element2 = dom.childAt(element1, [5]);
       var element3 = dom.childAt(element1, [9]);
       var element4 = dom.childAt(element1, [11]);
-      var element5 = dom.childAt(element1, [13]);
-      var morphs = new Array(13);
+      var element5 = dom.childAt(element1, [17]);
+      var morphs = new Array(14);
       morphs[0] = dom.createMorphAt(element1,1,1);
       morphs[1] = dom.createMorphAt(dom.childAt(element2, [3]),1,1);
       morphs[2] = dom.createMorphAt(dom.childAt(element2, [7]),1,1);
@@ -449,28 +495,30 @@ exports["default"] = Ember.HTMLBars.template((function() {
       morphs[4] = dom.createMorphAt(dom.childAt(element3, [7]),1,1);
       morphs[5] = dom.createMorphAt(dom.childAt(element4, [3]),1,1);
       morphs[6] = dom.createMorphAt(dom.childAt(element4, [7]),1,1);
-      morphs[7] = dom.createMorphAt(dom.childAt(element5, [3]),1,1);
-      morphs[8] = dom.createMorphAt(dom.childAt(element5, [7]),1,1);
-      morphs[9] = dom.createMorphAt(dom.childAt(element1, [17, 3]),1,1);
-      morphs[10] = dom.createMorphAt(element1,20,20);
-      morphs[11] = dom.createMorphAt(element0,4,4);
-      morphs[12] = dom.createMorphAt(element0,7,7);
+      morphs[7] = dom.createMorphAt(dom.childAt(element1, [13, 3]),1,1);
+      morphs[8] = dom.createMorphAt(dom.childAt(element5, [3]),1,1);
+      morphs[9] = dom.createMorphAt(dom.childAt(element5, [7]),1,1);
+      morphs[10] = dom.createMorphAt(dom.childAt(element1, [21, 3]),1,1);
+      morphs[11] = dom.createMorphAt(element1,24,24);
+      morphs[12] = dom.createMorphAt(element0,4,4);
+      morphs[13] = dom.createMorphAt(element0,7,7);
       return morphs;
     },
     statements: [
       ["inline","partial",["host/add-common"],[],["loc",[null,[3,4],[3,33]]],0,0],
       ["inline","input",[],["type","text","class","form-control","placeholder","Scaleway Organization ID","value",["subexpr","@mut",[["get","model.scalewayConfig.organization",["loc",[null,[16,18],[16,51]]],0,0,0,0]],[],[],0,0]],["loc",[null,[13,10],[17,12]]],0,0],
       ["inline","input",[],["type","text","class","form-control","placeholder","Scaleway API Token","value",["subexpr","@mut",[["get","model.scalewayConfig.token",["loc",[null,[26,18],[26,44]]],0,0,0,0]],[],[],0,0]],["loc",[null,[23,10],[27,12]]],0,0],
-      ["inline","input",[],["type","text","class","form-control","placeholder","Specify the image","value",["subexpr","@mut",[["get","model.scalewayConfig.image",["loc",[null,[42,18],[42,44]]],0,0,0,0]],[],[],0,0]],["loc",[null,[39,10],[43,12]]],0,0],
-      ["inline","input",[],["type","text","class","form-control","placeholder","Specify the commercial type","value",["subexpr","@mut",[["get","model.scalewayConfig.commercialType",["loc",[null,[52,18],[52,53]]],0,0,0,0]],[],[],0,0]],["loc",[null,[49,10],[53,12]]],0,0],
-      ["inline","input",[],["type","text","class","form-control","placeholder","Specify the location (par1,ams1)","value",["subexpr","@mut",[["get","model.scalewayConfig.region",["loc",[null,[64,18],[64,45]]],0,0,0,0]],[],[],0,0]],["loc",[null,[61,10],[65,12]]],0,0],
-      ["inline","input",[],["type","text","class","form-control","placeholder","Attach additional volume (e.g., 50G)","value",["subexpr","@mut",[["get","model.scalewayConfig.volumes",["loc",[null,[74,18],[74,46]]],0,0,0,0]],[],[],0,0]],["loc",[null,[71,10],[75,12]]],0,0],
-      ["inline","input",[],["type","text","class","form-control","placeholder","Optionally specify the IP Address","value",["subexpr","@mut",[["get","model.scalewayConfig.ip",["loc",[null,[86,21],[86,44]]],0,0,0,0]],[],[],0,0]],["loc",[null,[83,13],[87,15]]],0,0],
-      ["inline","input",[],["type","checkbox","checked",["subexpr","@mut",[["get","model.scalewayConfig.ipv6",["loc",[null,[94,23],[94,48]]],0,0,0,0]],[],[],0,0]],["loc",[null,[93,13],[95,15]]],0,0],
-      ["inline","input",[],["type","checkbox","checked",["subexpr","@mut",[["get","model.scalewayConfig.debug",["loc",[null,[109,20],[109,46]]],0,0,0,0]],[],[],0,0]],["loc",[null,[108,10],[110,12]]],0,0],
-      ["inline","partial",["host/add-options"],[],["loc",[null,[115,4],[115,34]]],0,0],
-      ["inline","top-errors",[],["errors",["subexpr","@mut",[["get","errors",["loc",[null,[119,22],[119,28]]],0,0,0,0]],[],[],0,0]],["loc",[null,[119,2],[119,30]]],0,0],
-      ["inline","save-cancel",[],["save","save","cancel","cancel"],["loc",[null,[122,2],[122,45]]],0,0]
+      ["inline","input",[],["type","text","class","form-control","placeholder","Optionally specify the instance name","value",["subexpr","@mut",[["get","model.scalewayConfig.name",["loc",[null,[42,16],[42,41]]],0,0,0,0]],[],[],0,0]],["loc",[null,[39,8],[43,10]]],0,0],
+      ["inline","input",[],["type","text","class","form-control","placeholder","Specify the location (par1,ams1)","value",["subexpr","@mut",[["get","model.scalewayConfig.region",["loc",[null,[52,16],[52,43]]],0,0,0,0]],[],[],0,0]],["loc",[null,[49,8],[53,10]]],0,0],
+      ["inline","input",[],["type","text","class","form-control","placeholder","Specify the image","value",["subexpr","@mut",[["get","model.scalewayConfig.image",["loc",[null,[64,18],[64,44]]],0,0,0,0]],[],[],0,0]],["loc",[null,[61,10],[65,12]]],0,0],
+      ["inline","input",[],["type","text","class","form-control","placeholder","Specify the commercial type","value",["subexpr","@mut",[["get","model.scalewayConfig.commercialType",["loc",[null,[74,18],[74,53]]],0,0,0,0]],[],[],0,0]],["loc",[null,[71,10],[75,12]]],0,0],
+      ["inline","input",[],["type","text","class","form-control","placeholder","Attach additional volume (e.g., 50G)","value",["subexpr","@mut",[["get","model.scalewayConfig.volumes",["loc",[null,[86,18],[86,46]]],0,0,0,0]],[],[],0,0]],["loc",[null,[83,10],[87,12]]],0,0],
+      ["inline","input",[],["type","text","class","form-control","placeholder","Optionally specify the IP Address","value",["subexpr","@mut",[["get","model.scalewayConfig.ip",["loc",[null,[102,21],[102,44]]],0,0,0,0]],[],[],0,0]],["loc",[null,[99,13],[103,15]]],0,0],
+      ["inline","input",[],["type","checkbox","checked",["subexpr","@mut",[["get","model.scalewayConfig.ipv6",["loc",[null,[110,23],[110,48]]],0,0,0,0]],[],[],0,0]],["loc",[null,[109,13],[111,15]]],0,0],
+      ["inline","input",[],["type","checkbox","checked",["subexpr","@mut",[["get","model.scalewayConfig.debug",["loc",[null,[124,20],[124,46]]],0,0,0,0]],[],[],0,0]],["loc",[null,[123,10],[125,12]]],0,0],
+      ["inline","partial",["host/add-options"],[],["loc",[null,[130,4],[130,34]]],0,0],
+      ["inline","top-errors",[],["errors",["subexpr","@mut",[["get","errors",["loc",[null,[134,22],[134,28]]],0,0,0,0]],[],[],0,0]],["loc",[null,[134,2],[134,30]]],0,0],
+      ["inline","save-cancel",[],["save","save","cancel","cancel"],["loc",[null,[137,2],[137,45]]],0,0]
     ],
     locals: [],
     templates: []
